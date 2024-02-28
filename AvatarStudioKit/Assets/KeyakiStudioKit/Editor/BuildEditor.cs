18604309c94aa58ce3ce59e50127e49500a82a45
Yuta Ueda <>
2024-02-28 13:05:09 +0900

    BuildEditorの修正

--- AvatarStudioKit/Assets/KeyakiStudioKit/Editor/BuildEditor.cs
+++ AvatarStudioKit/Assets/KeyakiStudioKit/Editor/BuildEditor.cs
@@ -13,16 +13,44 @@ namespace AvatarStudio
         {
             SetUp();
 
-            Build();
+            if (Selection.objects.Length > 0)
+            {
+                if (Selection.objects[0] is GameObject obj)
+                {
+                    AssetBuilder.Build(obj);
+                    return;
+                }
+                else if (Selection.objects[0] is SceneAsset scene)
+                {
+                    SceneBuilder.Build(scene);
+                    return;
+                }
+            }
+
+            Debug.LogError("[KeyakiStudioKit] Not Selected.");
         }
 
-        [MenuItem("GameObject/Asset Build", false, 0)]
-        static public void OnGameObject()
-        {
-            SetUp();
+        //[MenuItem("GameObject/Asset Build", false, 0)]
+        //static public void OnGameObject()
+        //{
+        //    SetUp();
 
-            Build();
-        }
+        //    if (Selection.objects.Length > 0)
+        //    {
+        //        if (Selection.objects[0] is GameObject obj)
+        //        {
+        //            AssetBuilder.Build(obj);
+        //            return;
+        //        }
+        //        else if (Selection.objects[0] is SceneAsset scene)
+        //        {
+        //            SceneBuilder.Build(scene);
+        //            return;
+        //        }
+        //    }
+
+        //    Debug.LogError("[KeyakiStudioKit] Not Selected.");
+        //}
 
         static void SetUp()
         {
@@ -35,19 +63,23 @@ namespace AvatarStudio
             if (!Directory.Exists(path))
                 Directory.CreateDirectory(path);
         }
+    }
 
-        static void Build()
+    #region -- Asset Build --
+
+    public class AssetBuilder
+    {
+        static public void Build(GameObject obj)
         {
-            var gameObject = Selection.objects[0] as GameObject;
-            var inputPath = AssetDatabase.GetAssetOrScenePath(gameObject);
+            var inputPath = AssetDatabase.GetAssetOrScenePath(obj);
 
             // Asset Prefab
-            var ap = gameObject.GetComponent<AssetPrefab>();
+            var ap = obj.GetComponent<AssetPrefab>();
             if (ap == null)
-                ap = gameObject.AddComponent<AssetPrefab>();
+                ap = obj.AddComponent<AssetPrefab>();
 
             // Property Window
-            var proptyModalWindow = ScriptableObject.CreateInstance<BuildPropertyEditor>();
+            var proptyModalWindow = ScriptableObject.CreateInstance<AssetBuildPropertyEditor>();
             proptyModalWindow.Show(inputPath);
 
             proptyModalWindow._completion = (outputPath, assetId) =>
@@ -57,11 +89,11 @@ namespace AvatarStudio
 
                 Directory.CreateDirectory(outputPath);
 
-                Build(inputPath, outputPath, assetId);
+                AssetBuild(inputPath, outputPath, assetId);
             };
         }
 
-        static void Build(string inputPath, string outputPath, string assetId)
+        static public void AssetBuild(string inputPath, string outputPath, string assetId)
         {
             var builds = new List<AssetBundleBuild>();
 
@@ -90,7 +122,7 @@ namespace AvatarStudio
             BuildPipeline.BuildAssetBundles(winPath, builds.ToArray(), BuildAssetBundleOptions.ChunkBasedCompression, BuildTarget.StandaloneWindows64);
         }
 
-        public class BuildPropertyEditor : EditorWindow
+        class AssetBuildPropertyEditor : EditorWindow
         {
             public Action<string, string> _completion;
 
@@ -145,4 +177,116 @@ namespace AvatarStudio
             }
         }
     }
+
+    #endregion
+
+    #region -- Scene Build --
+
+    public class SceneBuilder
+    {
+        static public void Build(SceneAsset scene)
+        {
+            var inputPath = AssetDatabase.GetAssetOrScenePath(scene);
+
+            // Property Window
+            var proptyModalWindow = ScriptableObject.CreateInstance<SceneBuildPropertyEditor>();
+            proptyModalWindow.Show(inputPath);
+
+            proptyModalWindow._completion = (outputPath, assetId) =>
+            {
+                // Save
+                EditorUtility.SetDirty(scene);
+
+                Directory.CreateDirectory(outputPath);
+
+                SceneBuild(inputPath, outputPath, assetId);
+            };
+        }
+
+        static public void SceneBuild(string inputPath, string outputPath, string assetId)
+        {
+            var builds = new List<AssetBundleBuild>();
+
+            if (!Directory.Exists(outputPath))
+                Directory.CreateDirectory(outputPath);
+
+            var build = new AssetBundleBuild();
+            build.assetBundleName = assetId.ToLower() + ".bundle";
+            build.assetNames = new string[1] { inputPath };
+            builds.Add(build);
+
+            var iOSPath = outputPath + "/iOS";
+            Directory.CreateDirectory(iOSPath);
+            BuildPipeline.BuildAssetBundles(iOSPath, builds.ToArray(), BuildAssetBundleOptions.ChunkBasedCompression, BuildTarget.iOS);
+
+            var androidPath = outputPath + "/Android";
+            Directory.CreateDirectory(androidPath);
+            BuildPipeline.BuildAssetBundles(androidPath, builds.ToArray(), BuildAssetBundleOptions.ChunkBasedCompression, BuildTarget.Android);
+
+            var osxPath = outputPath + "/StandaloneOSX";
+            Directory.CreateDirectory(osxPath);
+            BuildPipeline.BuildAssetBundles(osxPath, builds.ToArray(), BuildAssetBundleOptions.ChunkBasedCompression, BuildTarget.StandaloneOSX);
+
+            var winPath = outputPath + "/StandaloneWindows64";
+            Directory.CreateDirectory(winPath);
+            BuildPipeline.BuildAssetBundles(winPath, builds.ToArray(), BuildAssetBundleOptions.ChunkBasedCompression, BuildTarget.StandaloneWindows64);
+        }
+
+        class SceneBuildPropertyEditor : EditorWindow
+        {
+            public Action<string, string> _completion;
+
+            public string _outputPath;
+
+            public string _assetId = "";
+
+            public void Show(string inputPath)
+            {
+                _assetId = Path.GetFileNameWithoutExtension(inputPath);
+
+                ShowUtility();
+            }
+
+            void Awake()
+            {
+                titleContent = new GUIContent("Asset Build (Scene)");
+
+                minSize = new Vector2(320, 390);
+                position = new Rect(Vector2.zero, minSize);
+
+                _outputPath = Application.dataPath + "/KeyakiStudioKit/AssetBundles";
+            }
+
+            void OnGUI()
+            {
+                var titleTex = AssetDatabase.LoadAssetAtPath<Texture>("Assets/KeyakiStudioKit/Editor/title.png");
+                EditorGUILayout.LabelField(new GUIContent(titleTex), GUILayout.Height(320 * 1600f / 2400f), GUILayout.Width(320));
+
+                EditorGUILayout.LabelField("#1 Please enter the output directory path.", EditorStyles.wordWrappedLabel);
+
+                _outputPath = EditorGUILayout.TextField(_outputPath);
+
+                GUILayout.Space(20);
+
+                EditorGUILayout.LabelField("#2 Please enter asset name.", EditorStyles.wordWrappedLabel);
+
+                _assetId = EditorGUILayout.TextField(_assetId);
+
+                GUILayout.Space(20);
+
+                EditorGUILayout.BeginHorizontal();
+                {
+                    EditorGUILayout.Space();
+                    if (GUILayout.Button("Build", GUILayout.MaxWidth(200f)))
+                    {
+                        Close();
+                        _completion?.Invoke(_outputPath + "/" + _assetId, _assetId);
+                    }
+                    EditorGUILayout.Space();
+                }
+            }
+        }
+    }
+
+    #endregion
 }
