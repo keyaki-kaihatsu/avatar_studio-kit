8e2b8891b34dda3322db7b4bfad3489f458abf03
Yuta Ueda <>
2024-04-23 21:49:32 +0900

    partsオプションを追加

--- AvatarStudioKit/Assets/KeyakiStudioKit/Scripts/Models/Asset/AssetPrefab.cs
+++ AvatarStudioKit/Assets/KeyakiStudioKit/Scripts/Models/Asset/AssetPrefab.cs
@@ -4,5 +4,13 @@ namespace AvatarStudio
 {
     public class AssetPrefab : MonoBehaviour
     {
+        #region -- Public Property --
+
+        [Header("Options")]
+
+        [Tooltip("Objects you want to partially edit.")]
+        public GameObject[] parts;
+
+        #endregion
     }
 }
